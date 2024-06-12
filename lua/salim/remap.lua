15204659775ec3f5fa704bfbe32efc531588f520
= <=>
2024-06-12 15:10:31 +0100

    remap q: car c'est chiant

--- lua/salim/remap.lua
+++ lua/salim/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.api.nvim_set_keymap("n","<A-j>", ":m .+1<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n","<A-k>", ":m .-2<CR>", { noremap = true, silent = true })
@@ -26,28 +26,32 @@ vim.api.nvim_set_keymap("i","/*", "/**/<esc>hi", { noremap = true, silent = true
 
 vim.api.nvim_set_keymap("n","<leader>lm", ":Mason<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n","<leader>li", ":LspInfo<CR>", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap("n","<leader>pl", ":Lazy<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n","<leader>ps", ":Lazy sync<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n","q:", "", { noremap = true, silent = true })
 
 function _G.open_floating_lazygit()
-  local buf = vim.api.nvim_create_buf(false, true)
-  local width = math.ceil(vim.o.columns * 0.8)
-  local height = math.ceil(vim.o.lines * 0.8)
-  local opts = {
-    style = "minimal",
-    relative = "editor",
-    width = width,
-    height = height,
-    row = math.ceil((vim.o.lines - height) / 2),
-    col = math.ceil((vim.o.columns - width) / 2),
-  }
-  local win = vim.api.nvim_open_win(buf, true, opts)
-  vim.fn.termopen("lazygit", {
-    on_exit = function()
-      vim.api.nvim_win_close(win, true)
-    end
-  })
-  vim.cmd("startinsert")
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<cmd>close!<CR>', { noremap = true, silent = true })
+    local buf = vim.api.nvim_create_buf(false, true)
+    local width = math.ceil(vim.o.columns * 0.8)
+    local height = math.ceil(vim.o.lines * 0.8)
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = width,
+        height = height,
+        row = math.ceil((vim.o.lines - height) / 2),
+        col = math.ceil((vim.o.columns - width) / 2),
+    }
+    local win = vim.api.nvim_open_win(buf, true, opts)
+    vim.fn.termopen("lazygit", {
+        on_exit = function()
+            vim.api.nvim_win_close(win, true)
+        end
+    })
+    vim.cmd("startinsert")
+    vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<cmd>close!<CR>', { noremap = true, silent = true })
 end
 
 vim.api.nvim_set_keymap('n', '<leader>gl', ':lua open_floating_lazygit()<CR>', { noremap = true, silent = true })
