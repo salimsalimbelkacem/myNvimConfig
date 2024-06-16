bdab3fbddfcb03895508d0c239b7826b447eecd0
= <=>
2024-06-16 10:58:58 +0100

    undofile maintenant on peux undo comme on veux

--- lua/salim/init.lua
+++ lua/salim/init.lua
@@ -1,28 +1,36 @@
-require("salim.remap")
+vim.g.mapleader = " "
+
 require("salim.lazy_init")
--- TO DO:
--- [ ] cmp pour path
--- [x] enelever le simbole nule teh [+] sur lualine
--- [x] keymap pour fermer (tout) les buffers
+require("salim.remap")
+--[[ 
+ TO DO:
+- [ ]   on change de plugin pour dashboard vu
+            qu'on peux pas changer de couler.
+- [x]   enregistrer les undo
+ ]]
 
-vim.o.nu              = true
-vim.o.tabstop         = 4
-vim.o.softtabstop     = 4
-vim.o.shiftwidth      = 4
-vim.o.expandtab       = true
-vim.o.clipboard       = "unnamedplus"
-vim.o.signcolumn      = 'yes'
-vim.o.showmode        = false
-vim.o.swapfile        = false
-vim.o.backup          = false
-vim.o.termguicolors   = true
+vim.o.termguicolors  = true
+vim.o.signcolumn     = 'yes'
+vim.o.clipboard      = "unnamedplus"
+vim.o.showmode       = false
+vim.o.nu             = true
 
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldcolumn = '1'
-vim.foldlevel = 99
+vim.o.undofile       = true
+vim.o.undodir        = "/home/salim/.vim/vimundo"
+
+vim.o.softtabstop    = 4
+vim.o.shiftwidth     = 4
+vim.o.expandtab      = true
+vim.o.tabstop        = 4
+
+vim.o.swapfile       = false
+vim.o.backup         = false
+
 vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
-vim.o.fillchars = "fold: ,foldopen:,foldclose:,foldsep: "
+vim.o.foldcolumn     = '1'
+vim.o.foldmethod     = "expr"
+vim.o.foldenable     = true
+vim.o.fillchars      = "foldopen:,foldclose:,foldsep: "
+vim.o.foldexpr       = "nvim_treesitter#foldexpr()"
+vim.foldlevel        = 99
 
